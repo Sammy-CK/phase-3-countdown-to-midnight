97095757afa88278c996258fe8eae025834d00cc
Sammy-CK <sammycherono2696@gmail.com>
2023-02-08 12:03:04 +0300

    Completed task

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
 
-def countdown
+def countdown (word)
+    
+    while word > 0
+        puts "#{word} SECOND(S)!"
+        word -= 1
+    end
+
+    "HAPPY NEW YEAR!"
+
+
+end
+
+
+def countdown_with_sleep (num)
+    sleep (num)
+    countdown(num)
+    
 end
